b32b0dc4a406798197c9c79ebbae888c8e27085e
MINAKSHI KUMARI PATHAK <167210160+minu-a1@users.noreply.github.com>
2024-08-06 17:59:36 +0530

    Add files via upload
    One day I'd like to go to the Moon and look at the planet Earth and say, 'Wow, there's part of my portfolio.' I’m trying to manage myself, not just my portfolio. I'm looking to expand my portfolio while I'm on top and while I'm young. A good portfolio is more than a long list of good stocks and bonds.

---
+++ word-list.js
@@ -0,0 +1,85 @@
+const wordList = [
+    {
+        word: "theator",
+        hint: "today we are celebrating"
+    },
+    {
+        word: " rangmanch",
+        hint: "in hindi we called theator as"
+    },
+    {
+        word: "malam",
+        hint: "famous theator of uttrakhand "
+    },
+    {
+        word: "painting",
+        hint: "An art form using colors on a surface to create images or expression."
+    },
+    {
+        word: "camera",
+        hint: "device used to capture images."
+    },
+    
+    {
+        word: "chocolate",
+        hint: "A sweet treat made from cocoa beans."
+    },
+   
+    {
+        word: "singer",
+        hint: "peson who sing"
+    },
+    {
+        word: "diamond",
+        hint: "A precious gemstone known for its brilliance and hardness."
+    },
+    {
+        word: "adventure",
+        hint: "Theator."
+    },
+    {
+        word: "",
+        hint: "famous theator actor  of india ."
+    },
+    {
+        word: "dance",
+        hint: "A rhythmic movement of the body often performed to music."
+    },
+    {
+        word: "galaxy",
+        hint: "A vast system of stars, gas, and dust held together by gravity."
+    },
+   
+    {
+        word: "photography",
+        hint: "The art, process, or practice of creating images by recording light or other electromagnetic radiation."
+    },
+    {
+        word: "safari",
+        hint: "An expedition or journey, typically to observe wildlife in their natural habitat."
+    },
+    {
+        word: "planet",
+        hint: "A celestial body that orbits a star and does not produce light of its own."
+    },
+    {
+        word: "river",
+        hint: "A large natural stream of water flowing in a channel to the sea, a lake, or another such stream."
+    },
+    {
+        word: "moonlight",
+        hint: "The light from the moon."
+    },
+    {
+        word: "vibrant",
+        hint: "Full of energy, brightness, and life."
+    },
+    {
+        word: "nostalgia",
+        hint: "A sentimental longing or wistful affection for the past."
+    },
+    {
+        word: "brilliant",
+        hint: "Exceptionally clever, talented, or impressive."
+    },
+];
